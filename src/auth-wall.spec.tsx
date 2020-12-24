1bb105d7a55837dd5773e99d5fdff8b513400cf4
Max Prilutskiy <maks.prilutskiy@gmail.com>
2020-12-24 16:37:26 +0200

    chore: added more tests for auth-wall component

--- src/auth-wall.spec.tsx
+++ src/auth-wall.spec.tsx
@@ -3,9 +3,44 @@ import { AuthWall } from './auth-wall'
 import { render } from '@testing-library/react'
 
 describe('AuthWall', () => {
-  it('Should render without exceptions', () => {
+  it('Should render null and complain when authHook is not provided', () => {
+    const originalConsoleError = console.error;
+    const mockedConsoleError: typeof originalConsoleError = jest.fn(() => '')
+    console.error = mockedConsoleError;
+
     const wrapper = render(<AuthWall />)
 
-    expect(wrapper).toBeDefined()
+    expect(mockedConsoleError).toBeCalled();
+    expect(wrapper.container.firstChild).toBeNull();
+  });
+
+  it('Should render auth component when not authenticated', () => {
+    const authHook = jest.fn(() => ({ isLoading: false, data: null }))
+    const authComponent = jest.fn(() => <span id="auth-component" />)
+
+    const wrapper = render(
+      <AuthWall
+        authHook={authHook}
+        authComponent={authComponent}
+      />
+    );
+
+    expect(authComponent).toBeCalled()
+    expect(wrapper.container.querySelector('#auth-component')).not.toBeNull()
+  });
+
+  it('Should render app component when authenticated', () => {
+    const authHook = jest.fn(() => ({ isLoading: false, data: {} }))
+    const appComponent = jest.fn(() => <span id="app-component" />)
+
+    const wrapper = render(
+      <AuthWall
+        authHook={authHook}
+        appComponent={appComponent}
+      />
+    );
+
+    expect(appComponent).toBeCalled()
+    expect(wrapper.container.querySelector('#app-component')).not.toBeNull()
   });
 });
